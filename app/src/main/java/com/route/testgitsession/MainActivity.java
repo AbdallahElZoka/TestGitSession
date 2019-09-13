eac041a1554f20698abf4dfb6b64578fd38acfe1
peter <petersalahsamir@gmail.com>
2019-09-13 21:36:15 +0200

    change click lisntener to toast

--- app/src/main/java/com/route/testgitsession/MainActivity.java
+++ app/src/main/java/com/route/testgitsession/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                button.setText("Changed");
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
